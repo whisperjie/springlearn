4cfa6c9a4ae006e70c5dbeeed6ac2dba6c944640
whisper <1416207703@qq.com>
2019-03-25 21:17:56 +0800

    init add hello controller

---
+++ src/main/java/com/whisper/mmal/MmalApplication.java
@@ -0,0 +1,13 @@
+package com.whisper.mmal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmalApplication.class, args);
+    }
+
+}
